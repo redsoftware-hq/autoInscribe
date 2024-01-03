a4f410fa0ebdd21b97b6c5b943dfa5197440e7d1
Ashish Baravaliya <vier.institute35@gmail.com>
2024-01-02 20:09:50 -0500

    fix: hide password hint and progress

--- autoinscribe/autoinscribe/doctype/autoinscribe_settings/autoinscribe_settings.js
+++ autoinscribe/autoinscribe/doctype/autoinscribe_settings/autoinscribe_settings.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2023, RedSoft Solutions Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("AutoInscribe Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("AutoInscribe Settings", {
+  refresh: function (frm) {
+    hidePasswordStrengthElements(frm);
+  },
+});
 
-// 	},
-// });
+function hidePasswordStrengthElements(frm) {
+  $.each(frm.fields_dict, function (fieldname, field) {
+    if (field.df && field.df.fieldtype === "Password") {
+      field.$wrapper.find(".password-strength-indicator, .help-box").hide();
+    }
+  });
+}
